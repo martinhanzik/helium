8308a956894896c760641c3d8cc4bfef3c7688f2
Martin Hanzík <martin@hanzik.com>
2017-01-14 21:17:40 +0100

    Try using glium_text.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-extern crate sdl2;
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+
+
+extern crate glium;
+extern crate glium_text;
+extern crate cgmath;
 #[macro_use]
 extern crate clap;
 
 use std::io::Read;
 use std::fs::File;
+use std::time::Duration;
+use std::thread;
 use clap::{Arg, App};
 
-struct Line {
-    rect: sdl2::rect::Rect,
-    texture: sdl2::render::Texture,
-}
+use glium::{DisplayBuild, Surface};
+use glium::glutin;
 
 fn main() {
     let matches = App::new("Helium")
@@ -18,64 +24,87 @@ fn main() {
         .about("The awesomest of editors.")
         .arg(Arg::with_name("file")
             .help("Input file")
-            .required(true)
             .index(1))
         .get_matches();
 
     let mut string = String::new();
-    File::open(matches.value_of("file").unwrap())
-        .expect("Failed to open file")
-        .read_to_string(&mut string)
-        .expect("Failed to read file.");
 
-    let sdl_context = sdl2::init().unwrap();
-    let ttf_context = sdl2::ttf::init().unwrap();
-    let window = sdl_context.video()
-        .unwrap()
-        .window("Helium", 800, 600)
-        .resizable()
-        .allow_highdpi()
-        .build()
-        .unwrap();
-    let mut renderer = window.renderer()
-        .accelerated()
-        .present_vsync()
-        .build()
-        .unwrap();
-    let font = ttf_context.load_font(std::path::Path::new("DejaVuSansMono.ttf"), 24).unwrap();
-
-    let mut textures = vec![];
-
-    for (index, line) in string.lines().enumerate() {
-        if line.is_empty() {
-            continue;
-        }
-        let line_render = font.render(&line[..]).blended(sdl2::pixels::Color::RGB(150, 150, 150)).unwrap();
-        let rect = sdl2::rect::Rect::new(0,
-                                         index as i32 * line_render.height() as i32,
-                                         line_render.width(),
-                                         line_render.height());
-        let texture = renderer.create_texture_from_surface(&line_render).unwrap();
-        textures.push(Line {
-            rect: rect,
-            texture: texture,
-        });
+    if let Some(file) = matches.value_of("file") {
+        println!("Reading file: {}", file);
+        File::open(file)
+            .expect("Failed to open file")
+            .read_to_string(&mut string)
+            .expect("Failed to read file.");
     }
 
 
-    renderer.set_draw_color(sdl2::pixels::Color::RGB(30, 35, 40));
+    let display = glutin::WindowBuilder::new()
+        .with_vsync()
+        .with_dimensions(800, 600)
+        .with_title("Helium")
+        .build_glium()
+        .unwrap();
+    let system = glium_text::TextSystem::new(&display);
 
-    for event in sdl_context.event_pump().unwrap().wait_iter() {
-        match event {
-            sdl2::event::Event::Quit { .. } => return,
-            sdl2::event::Event::Window { .. } => {
-                renderer.clear();
-                for line in textures.iter_mut() {
-                    renderer.copy(&mut line.texture, None, Some(line.rect)).unwrap();
+    let font = glium_text::FontTexture::new(&display, &include_bytes!("../DejaVuSansMono.ttf")[..], 70).unwrap();
+
+    let mut buffer = String::new();
+
+    let sleep_duration = Duration::from_millis(17);
+
+    // let mut text: String = "A japanese poem:\r
+    // \r
+    // 色は匂へど散りぬるを我が世誰ぞ常ならむ有為の奥山今日越えて浅き夢見じ酔ひもせず\r
+    // \r
+    // Feel free to type out some text, and delete it with Backspace. You can also try resizing this window."
+    //         .into();
+
+    'main: loop {
+
+        let text = glium_text::TextDisplay::new(&system, &font, &buffer);
+
+        let (w, h) = display.get_framebuffer_dimensions();
+
+        let matrix: [[f32; 4]; 4] = cgmath::Matrix4::new(0.1,
+                                                         0.0,
+                                                         0.0,
+                                                         0.0,
+                                                         0.0,
+                                                         0.1 * (w as f32) / (h as f32),
+                                                         0.0,
+                                                         0.0,
+                                                         0.0,
+                                                         0.0,
+                                                         1.0,
+                                                         0.0,
+                                                         -0.9,
+                                                         0.0,
+                                                         0.0,
+                                                         1.0f32)
+            .into();
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 1.0);
+        glium_text::draw(&text, &system, &mut target, matrix, (1.0, 1.0, 0.0, 1.0));
+        target.finish().unwrap();
+
+        thread::sleep(sleep_duration);
+
+
+        for event in display.poll_events() {
+            match event {
+                glutin::Event::KeyboardInput(_, _, Some(glutin::VirtualKeyCode::Escape)) |
+                glutin::Event::Closed => break 'main,
+                glutin::Event::ReceivedCharacter(c) => {
+                    if c != '\u{7f}' && c != '\u{8}' {
+                        buffer.push(c);
+                    }
                 }
-                renderer.present();
+                glutin::Event::KeyboardInput(glutin::ElementState::Pressed, _, Some(glutin::VirtualKeyCode::Back)) => {
+                    buffer.pop();
+                }
+                _ => {}
             }
-            _ => {}
         }
     }
 }
