4360390f18ec0a914e782a67266173782b6757c6
Martin Hanzík <martin@hanzik.com>
2017-02-28 18:25:55 +0100

    Fix.

--- ci/install.sh
+++ ci/install.sh
@@ -1,23 +1,24 @@
 set -ex
 
 main() {
+  pwd
   wget https://www.libsdl.org/release/SDL2-2.0.4.tar.gz -O sdl2.tar.gz
   tar xzf sdl2.tar.gz
 
-  pushd SDL2-2.0.4
+  cd SDL2-2.0.4
   ./configure
   make
   sudo make install
-  pop
+  cd ..
 
   wget -q http://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.12.tar.gz
   tar xzf SDL2_ttf-*.tar.gz
 
-  pushd SDL2_ttf-*
+  cd SDL2_ttf-*
   ./configure
   make
   sudo make install
-  popd
+  cd ..
 }
 
 main
