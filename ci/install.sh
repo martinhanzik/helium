678f02a320100b981f6bbcd607cf256ff4b79927
Martin Hanzík <martin@hanzik.com>
2017-03-04 23:20:31 +0100

    Build deps only when necessasry.

--- ci/install.sh
+++ ci/install.sh
@@ -2,26 +2,29 @@ set -ex
 
 main() {
   if [ $TRAVIS_OS_NAME = linux ]; then
+      mkdir -p deps
+      cd deps
+      if [ ! -d "SDL2-2.0.4" ]; then
+        wget https://www.libsdl.org/release/SDL2-2.0.4.tar.gz -O sdl2.tar.gz
+        tar xzf sdl2.tar.gz
 
-    mkdir -p deps
-    cd deps
-    wget https://www.libsdl.org/release/SDL2-2.0.4.tar.gz -O sdl2.tar.gz
-    tar xzf sdl2.tar.gz
+        cd SDL2-*
+        ./configure
+        make
+        sudo make install
+        cd ..
+      fi
 
-    cd SDL2-2.0.4
-    ./configure
-    make
-    sudo make install
-    cd ..
+      if [ ! -d "SDL2_ttf-2.0.12" ]; then
+        wget -q http://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.12.tar.gz
+        tar xzf SDL2_ttf-*.tar.gz
 
-    wget -q http://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.12.tar.gz
-    tar xzf SDL2_ttf-*.tar.gz
-
-    cd SDL2_ttf-*
-    ./configure
-    make
-    sudo make install
-    cd ..
+        cd SDL2_ttf-*
+        ./configure
+        make
+        sudo make install
+        cd ..
+      fi
   else
     brew install freetype sdl2 sdl2_ttf
   fi
