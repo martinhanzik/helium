07a84928c967cdbd49a01cb1fdc3be622f7e45d1
Martin Hanzík <martin@hanzik.com>
2017-03-04 22:53:18 +0100

    Use brew.

--- ci/install.sh
+++ ci/install.sh
@@ -1,25 +1,30 @@
 set -ex
 
 main() {
-  mkdir -p deps
-  cd deps
-  wget https://www.libsdl.org/release/SDL2-2.0.4.tar.gz -O sdl2.tar.gz
-  tar xzf sdl2.tar.gz
+  if [ $TRAVIS_OS_NAME = linux ]; then
 
-  cd SDL2-2.0.4
-  ./configure
-  make
-  sudo make install
-  cd ..
+    mkdir -p deps
+    cd deps
+    wget https://www.libsdl.org/release/SDL2-2.0.4.tar.gz -O sdl2.tar.gz
+    tar xzf sdl2.tar.gz
 
-  wget -q http://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.12.tar.gz
-  tar xzf SDL2_ttf-*.tar.gz
+    cd SDL2-2.0.4
+    ./configure
+    make
+    sudo make install
+    cd ..
 
-  cd SDL2_ttf-*
-  ./configure
-  make
-  sudo make install
-  cd ..
+    wget -q http://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.12.tar.gz
+    tar xzf SDL2_ttf-*.tar.gz
+
+    cd SDL2_ttf-*
+    ./configure
+    make
+    sudo make install
+    cd ..
+  else
+    brew install freetype sdl2 sdl2_ttf
+  fi
 }
 
 main
